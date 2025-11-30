939548ed5d4ff627a2f058e74c31861ba42c65fe
Marko Pintera <bearishsun@gmail.com>
2025-11-30 13:30:44 +0100

    Refactor: Renamed & Moved RenderableUniformBufferManager files

--- Source/Plugins/bsfRenderBeast/Utility/B3DRenderableUniformBufferManager.cpp
+++ Source/Plugins/bsfRenderBeast/Utility/B3DRenderableUniformBufferManager.cpp
@@ -1,8 +1,8 @@
 //************************************ B3D Framework - Copyright 2025 Marko Pintera **************************************//
 //*********** Licensed under the MIT license. See LICENSE.md for full terms. This notice is not to be removed. ***********//
-#include "B3DPerObjectBufferManager.h"
-#include "B3DRendererRenderable.h"
-#include "B3DRendererDecal.h"
+#include "B3DRenderableUniformBufferManager.h"
+#include "../B3DRendererRenderable.h"
+#include "../B3DRendererDecal.h"
 
 using namespace b3d;
 using namespace b3d::render;
