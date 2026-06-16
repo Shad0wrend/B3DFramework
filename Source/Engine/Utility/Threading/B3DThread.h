495a32a550a7c8f38d3d780f0ddef60c0908dcc9
Marko Pintera <bearishsun@gmail.com>
2026-06-16 10:22:36 +0200

    Refactor: Moved thread logic into Platform subfolders, moved away from standard library threading primitives on all platforms

--- Source/Engine/Utility/Threading/B3DThread.h
+++ Source/Engine/Utility/Threading/B3DThread.h
@@ -44,7 +44,7 @@ namespace b3d
 	{
 	public:
 		/** True if thread affinity is supported on the current platform. */
-#if B3D_PLATFORM_WIN32 || B3D_PLATFORM_MACOS || B3D_PLATFORM_LINUX
+#if B3D_PLATFORM_WIN32 || B3D_PLATFORM_MACOS || B3D_PLATFORM_LINUX || B3D_PLATFORM_PS5
 		static constexpr bool kIsSupported = true;
 #else
 		static constexpr bool kIsSupported = false;
